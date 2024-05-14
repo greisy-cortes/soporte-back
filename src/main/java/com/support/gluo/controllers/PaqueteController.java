34eb67a2a996cc152f04467dcdb770c5bff1ca48
greisy-cortes <greisy@gluo.mx>
2024-05-14 10:10:27 -0600

    Se agregaron cambios para corregir error de consulta

--- src/main/java/com/support/gluo/controllers/PaqueteController.java
+++ src/main/java/com/support/gluo/controllers/PaqueteController.java
@@ -1,15 +1,12 @@
 package com.support.gluo.controllers;
 
-import com.support.gluo.dto.PaqueteDTO;
-import com.support.gluo.dto.Response;
 import com.support.gluo.services.ApiNubeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/api/roaming")
@@ -19,7 +16,7 @@ public class PaqueteController {
     ApiNubeService apiService;
 
     @GetMapping(value = "/tarifasTerrestre")
-    public Mono<Object> getRoamingTarifasTerrestre(){
+    public Mono<ResponseEntity<String>> getRoamingTarifasTerrestre(){
 
     return apiService.getRoamingTerrestre();
     }
--- src/main/java/com/support/gluo/services/ApiNubeService.java
+++ src/main/java/com/support/gluo/services/ApiNubeService.java
@@ -1,15 +1,11 @@
 package com.support.gluo.services;
 
-import com.support.gluo.dto.PaqueteDTO;
-import com.support.gluo.dto.Response;
 import com.support.gluo.externalComponents.ExternalAPIClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ApiNubeService {
@@ -22,8 +18,8 @@ public class ApiNubeService {
     public ApiNubeService(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("http://api-nube.telcel.com").build();
     }
-    public Mono<Object> getRoamingTerrestre(){
-        return this.webClient.get().uri("/telcel-api-web/api/roaming/paquetesTarifasTerrestre/2/1").retrieve().bodyToMono(Object.class);
+    public Mono<ResponseEntity<String>> getRoamingTerrestre(){
+        return this.webClient.get().uri("/telcel-api-web/api/roaming/paquetesTarifasTerrestre/2/1").retrieve().toEntity(String.class);
     }
 
 }
