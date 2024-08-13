0df8b22235148d324b4db096d7efcf530fd9e934
greisy-cortes <greisy@gluo.mx>
2024-08-13 12:21:23 -0600

    Se cambia la estructura para consultar a 2 hosts diferentes pero a los mismos servicios

--- src/main/java/com/support/gluo/services/ApiNubeService.java
+++ src/main/java/com/support/gluo/services/ApiNubeService.java
@@ -13,29 +13,27 @@ import java.util.List;
 @Service
 public class ApiNubeService {
 
-    RestClient restClient = RestClient.create("http://api-nube.telcel.com/telcel-api-web/api/roaming");
-
     private final RestTemplate restTemplate;
     public ApiNubeService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
-    public List<ResponseDTO> consultarServiciosApiNube(){
-
+    public List<ResponseDTO> consultarServiciosApiNube(String clientUrl){
+        RestClient restClient = RestClient.create(clientUrl);
         List<ResponseDTO> responses = new ArrayList<>();
 
-        responses.add(newService("/paquetesTarifasTerrestre/2/2", "Roaming Terrestre"));
-        responses.add(newService("/paquetesTarifasMaritima/2/2", "Roaming Maritima"));
-        responses.add(newService("/paquetesTarifasAerea/2/2", "Roaming Aerea"));
-        responses.add(newService("/paisesConPaquetesyTarifas/9", "Países con paquetes y tarifas"));
-        responses.add(newService("/lineasConPaquetesyTarifas/3/2", "Navieras y Aerolíneas"));
-        responses.add(newService("/trasporteConPaquetesyTarifas/1/2", "Barcos y Aviones"));
+        responses.add(newService(restClient, "/paquetesTarifasTerrestre/2/2", "Roaming Terrestre"));
+        responses.add(newService(restClient, "/paquetesTarifasMaritima/2/2", "Roaming Maritima"));
+        responses.add(newService(restClient, "/paquetesTarifasAerea/2/2", "Roaming Aerea"));
+        responses.add(newService(restClient, "/paisesConPaquetesyTarifas/9", "Países con paquetes y tarifas"));
+        responses.add(newService(restClient, "/lineasConPaquetesyTarifas/3/2", "Navieras y Aerolíneas"));
+        responses.add(newService(restClient, "/trasporteConPaquetesyTarifas/1/2", "Barcos y Aviones"));
 
         return responses;
     }
 
-    public ResponseDTO newService(String uri, String serviceName){
+    public ResponseDTO newService(RestClient client, String uri, String serviceName){
         try {
-            ResponseEntity<String> servicioTerrestre = restClient.get()
+            ResponseEntity<String> servicioTerrestre = client.get()
                     .uri(uri)
                     .retrieve()
                     .toEntity(String.class);
